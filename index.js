02fccd1d128cdd267a3a992bdb723d1ce859ed0b
roberto <fabio@fabionardelli.home-life.hub>
2020-05-04 15:24:22 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: __dirname + '/.env' });
+// require('dotenv').config({ path: __dirname + '/.env' });
 const { ApolloServer, PubSub } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const express = require('express');
