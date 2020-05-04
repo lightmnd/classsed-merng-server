20f9113da9e818fd84ff5f7d64527f720546a836
roberto <fabio@fabionardelli.home-life.hub>
2020-05-04 15:27:26 +0200

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: __dirname + '/.env' });
+//require('dotenv').config({ path: __dirname + '/.env' });
 module.exports = {
 	MONGODB: 'mongodb://lightmnd:Agata2009|@ds161894.mlab.com:61894/merng',
 	SECRET_KEY: process.env.SECRET
