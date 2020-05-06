c48b84b6922ef9ccc60563a9c54fa35fd2a1b159
roberto <fabio@fabionardelli.home-life.hub>
2020-05-06 12:08:56 +0200

    set secret key as string

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
 	MONGODB: 'mongodb://lightmnd:Agata2009|@ds161894.mlab.com:61894/merng',
 	//	MONGODB: 'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
-	SECRET_KEY: 1234567890 //process.env.SECRET
+	SECRET_KEY: '1234567890' //process.env.SECRET
 };
